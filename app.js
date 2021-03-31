32b30637aee2f54ffa340617bca8c45f2f999e72
JayTheMate <julenius.jesse@gmail.com>
2021-03-31 15:08:45 +0300

    Added set cookie and delete cookie

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
   res.render('home');
 });
+
+app.get('/setCookie/:clr', ((req, res) => {
+  res.cookie('color', req.params.clr, {httpOnly: true}).send('cookie set');
+}))
+
+app.get('/deleteCookie', ((req, res) => {
+  res.clearCookie('colorr');
+  res.send('cookie deleted');
+}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
