b7da9eafd2e9e5c3f6fb9e929349b8b3b5a49b80
JayTheMate <julenius.jesse@gmail.com>
2021-04-07 13:03:27 +0300

    added local strategy

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const passport = require('./utils/pass');
 const port = 3000;
+
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
+
+
 
 // don't do this in the project
 
@@ -21,21 +32,21 @@ app.use(session({
   cookie: {maxAge: 60*60*24},
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());// don't do this in the project
+
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 // don't do this in the project
-app.post('/login', (req, res) => {
-  const uname = req.body.username;
-  const passwd = req.body.password;
-  if (uname === username && passwd === password) {
-    req.session.kirjautunut = true;
-    res.redirect('/secret');
-  } else {
-    res.redirect('/form');
-  }
-});
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/form'}),
+    (req, res) => {
+      console.log('success');
+      res.redirect('/secret');
+    });
+
 // don't do this in the project
 app.get('/home', (req, res) => {
   res.render('home');
@@ -45,27 +56,30 @@ app.get('/form', (req, res) => {
   res.render('form');
 });
 // don't do this in the project
-app.get('/secret', (req, res) => {
-  if (req.session.kirjautunut) {
-    res.render('secret');
-  } else {
-    res.redirect('/form')
-  }
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
 });
 // don't do this in the project
 app.get('/setCookie/:clr', ((req, res) => {
   res.cookie('color', req.params.clr, {httpOnly: true}).send('cookie set');
-}))
+}));
 // don't do this in the project
 app.get('/readCookie', ((req, res) => {
   console.log('Cookies: ', req.cookies.color);
   res.send('cookie read');
-}))
+}));
 
 // don't do this in the project
 app.get('/deleteCookie', ((req, res) => {
   res.clearCookie('color');
   res.send('cookie deleted');
-}))
+}));
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
